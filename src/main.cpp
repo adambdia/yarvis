c2fea124ac4fd9fe7c4d44487e2660b9f92ded04
Adam Dia <adambilaldia@gmail.com>
2024-07-02 11:03:42 -0400

    modified gitignore, played with callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,34 @@
 
 
 bool protonect_shutdown = false; // Whether the running application should shut down.
-double frameGamma = 4096.0f;
-double maxGamma = 32768.0f, minGamma = 4096.0f;
-int sliderMax = 100;
+int sliderMax = 255;
 int sliderVal;
+
+typedef void(*frameCallback)(cv::Mat, void*);
 
 void sigint_handler(int s)
 {
   protonect_shutdown = true;
 }
 
-void updateGamma(int, void*)
+void frameHandler(cv::Mat receivedFrame, void* data)
 {
-    frameGamma = (double)( maxGamma - (maxGamma-minGamma)/sliderVal); 
+    cv::Mat segmented;
+    double max = 255.0;
+    double threshold = (double)*((int*)data);
 
+    cv::normalize(receivedFrame, segmented, 0, 255, cv::NORM_MINMAX);
+    segmented.convertTo(segmented, CV_8UC1);
+    cv::threshold(segmented, segmented, threshold, max, cv::THRESH_TOZERO);
+    cv::imshow("yarvis", segmented);
 }
+
 int main()
 {
     libfreenect2::Freenect2 freenect2;
     libfreenect2::Freenect2Device *dev = 0;
     libfreenect2::PacketPipeline *pipeline = 0;
-
+    std::cout << "what it do flight crew" << std::endl;
     if (freenect2.enumerateDevices() == 0)
     {
         std::cout << "no device connected!" << std::endl;
@@ -45,15 +52,21 @@ int main()
 
     signal(SIGINT, sigint_handler);
 
-    libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Ir | libfreenect2::Frame::Color);
+    libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Ir);
     libfreenect2::FrameMap frames;
 
     dev->setIrAndDepthFrameListener(&listener);
-    dev->setColorFrameListener(&listener);
+    //dev->setColorFrameListener(&listener);
 
     if (!dev->start()) return -1;
 
     cv::Mat colorFrame, irFrame;
+
+    std::vector<frameCallback> frameCallbacks;
+    frameCallbacks.push_back(frameHandler);
+
+    cv::namedWindow("yarvis", cv::WINDOW_AUTOSIZE);
+    cv::createTrackbar("treshold", "yarvis", &sliderVal, sliderMax, NULL, NULL);
  
     while (!protonect_shutdown )
     {
@@ -62,16 +75,20 @@ int main()
             std::cout << "timeout!" << std::endl;
             return -1;
         }
-
         libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-        libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];
+        //libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];
 
         cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irFrame);
-        cv::Mat(color->height, color->width, CV_8UC4, color->data).copyTo(colorFrame);
+        //cv::Mat(color->height, color->width, CV_8UC4, color->data).copyTo(colorFrame);
 
+        //irFrame = irFrame / 32768.0;
+        //cv::imshow("ir", irFrame);
+        //cv::imshow("color", colorFrame);
 
-        cv::imshow("ir", irFrame / 32768.0);
-        cv::imshow("color", colorFrame);
+        for(frameCallback& callback : frameCallbacks)
+        {
+            callback(irFrame, &sliderVal);
+        }
         cv::waitKey(1);
 
         listener.release(frames);
