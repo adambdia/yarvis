fc707996e85c0d9aaa08673a09cd0f9331e860e6
Adam Dia <adambilaldia@gmail.com>
2024-11-01 22:26:31 -0400

    started python interface

--- src/kinect.cpp
+++ src/kinect.cpp
@@ -11,12 +11,6 @@ bool protonect_shutdown = false; // Whether the running application should shut 
 int sliderMax = 255;
 int sliderVal;
 
-
-void sigint_handler(int s)
-{
-  protonect_shutdown = true;
-}
-
 int main()
 {
     libfreenect2::Freenect2 freenect2;
@@ -37,7 +31,6 @@ int main()
         return -1;
     }
 
-    signal(SIGINT, sigint_handler);
     libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Ir);
     libfreenect2::FrameMap frames;
 
@@ -48,25 +41,16 @@ int main()
 
     cv::Mat irFrame;
 
-
-    while (!protonect_shutdown )
+    if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 seconds
     {
-        if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 seconds
-        {
-            std::cout << "timeout!" << std::endl;
-            return -1;
-        }
-        libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-        libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];
-
-        cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irFrame);
-
-        cv::imshow("ir", irFrame / 65536.0);
-        
-        cv::waitKey(1);
-
-        listener.release(frames);
+        std::cout << "timeout!" << std::endl;
+        return -1;
     }
+    libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
+    libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];    cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irFrame);    cv::imshow("ir", irFrame / 65536.0);
+    
+    cv::waitKey(1);    listener.release(frames);
+
 
     dev->stop();
     dev->close();
