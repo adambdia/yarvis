89b951ac78b220a67c6a5e64c90120c7f1b3efe1
Adam Dia <adambilaldia@gmail.com>
2024-11-01 21:48:28 -0400

    removed unnessary from c++

--- src/main.cpp
+++ src/main.cpp
@@ -11,31 +11,17 @@ bool protonect_shutdown = false; // Whether the running application should shut 
 int sliderMax = 255;
 int sliderVal;
 
-typedef void(*frameCallback)(cv::Mat, void*);
 
 void sigint_handler(int s)
 {
   protonect_shutdown = true;
 }
 
-void frameHandler(cv::Mat receivedFrame, void* data)
-{
-    cv::Mat segmented;
-    double max = 255.0;
-    double threshold = (double)*((int*)data);
-    
-    cv::normalize(receivedFrame, segmented, 0, 255, cv::NORM_MINMAX);
-    segmented.convertTo(segmented, CV_8UC1);
-    cv::threshold(segmented, segmented, threshold, max, cv::THRESH_TOZERO);
-    cv::imshow("yarvis", segmented);
-}
-
 int main()
 {
     libfreenect2::Freenect2 freenect2;
     libfreenect2::Freenect2Device *dev = 0;
-    libfreenect2::PacketPipeline *pipeline = 0;
-    std::cout << "hello" << std::endl;
+
     if (freenect2.enumerateDevices() == 0)
     {
         std::cout << "no device connected!" << std::endl;
@@ -52,23 +38,17 @@ int main()
     }
 
     signal(SIGINT, sigint_handler);
-
     libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Ir);
     libfreenect2::FrameMap frames;
 
     dev->setIrAndDepthFrameListener(&listener);
-    //dev->setColorFrameListener(&listener);
+    dev->setColorFrameListener(&listener);
 
     if (!dev->start()) return -1;
 
-    cv::Mat colorFrame, irFrame;
+    cv::Mat irFrame;
 
-    std::vector<frameCallback> frameCallbacks;
-    frameCallbacks.push_back(frameHandler);
 
-    cv::namedWindow("yarvis", cv::WINDOW_AUTOSIZE);
-    cv::createTrackbar("treshold", "yarvis", &sliderVal, sliderMax, NULL, NULL);
- 
     while (!protonect_shutdown )
     {
         if (!listener.waitForNewFrame(frames, 10 * 1000)) // 10 seconds
@@ -77,19 +57,12 @@ int main()
             return -1;
         }
         libfreenect2::Frame *ir = frames[libfreenect2::Frame::Ir];
-        //libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];
+        libfreenect2::Frame *color = frames[libfreenect2::Frame::Color];
 
         cv::Mat(ir->height, ir->width, CV_32FC1, ir->data).copyTo(irFrame);
-        //cv::Mat(color->height, color->width, CV_8UC4, color->data).copyTo(colorFrame);
 
-        //irFrame = irFrame / 32768.0;
-        //cv::imshow("ir", irFrame);
-        //cv::imshow("color", colorFrame);
-
-        for(frameCallback& callback : frameCallbacks)
-        {
-            callback(irFrame, &sliderVal);
-        }
+        cv::imshow("ir", irFrame);
+        
         cv::waitKey(1);
 
         listener.release(frames);
