0e9000c67ff20e08807d50a67ad7ceba45804e6d
Adam Dia <adambilaldia@gmail.com>
2024-11-03 16:17:45 -0500

    removed bgr from python

--- python/main.py
+++ python/main.py
@@ -13,11 +13,11 @@ def main():
             try:
                 # Get frames from Kinect
                 frames = kinect.get_frames()
-                bgr_frame = frames['bgr']
+                # bgr_frame = frames['bgr']
                 depth_frame = frames['depth']
                 
                 # Display the frames
-                cv2.imshow('RGB', bgr_frame)
+                #cv2.imshow('RGB', bgr_frame)
                 cv2.imshow('Depth', depth_frame / 4500.0)  # Normalize depth for visualization
                 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
