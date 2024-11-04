39f14d6d4991e8eb6c77365a7aac16dd5d1fc76d
dooplu <adambilaldia@gmail.com>
2024-11-03 21:52:01 -0500

    works

--- python/main.py
+++ python/main.py
@@ -1,18 +1,7 @@
 import cv2
 from kinect_bridge import KinectBridge
-import sys
 import traceback
-import mediapipe as mp
 import numpy as np
-
-# Initialize MediaPipe
-mp_pose = mp.solutions.pose
-pose = mp_pose.Pose(static_image_mode=False, 
-                    model_complexity=1,
-                    min_detection_confidence=0.5)
-mp_draw = mp.solutions.drawing_utils
-
-
 
 def main():
     try:
@@ -28,23 +17,11 @@ def main():
                 depth_frame = frames['depth']
                 ir_frame = frames['ir']
                 
-                # Display the frames
-                #cv2.imshow('RGB', bgr_frame)
-                #cv2.imshow('Depth', depth_frame / 4500.0)  # Normalize depth for visualization
-                #cv2.imshow('ir', ir_frame / 65536.0)
-                #ir_frame = ir_frame / 65536.0
+                ir_frame = ir_frame / 256.0
                 ir_frame = ir_frame.astype(np.uint8)
 
-                # results = pose.process(cv2.cvtColor(ir_frame, cv2.COLOR_GRAY2RGB))
-
-                # if results.pose_landmarks:
-                #     mp_draw.draw_landmarks(
-                #     ir_frame,
-                #     results.pose_landmarks,
-                #     mp_pose.POSE_CONNECTIONS
-                #     )
-
-                cv2.imshow('Kinect MediaPipe', ir_frame)
+                cv2.imshow('Depth', depth_frame / 4500.0)  # Normalize depth for visualization
+                cv2.imshow('IR', ir_frame)
 
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
