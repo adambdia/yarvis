d9d8a3c815a6705a12c13fa27dc353e2a2874d9b
Adam Dia <adambilaldia@gmail.com>
2024-11-23 00:49:48 -0500

    event name checking for write_event

--- python/event_manager.py
+++ python/event_manager.py
@@ -4,10 +4,11 @@ class Event_Manager():
         self.events = {}
         
     def read_event(self, event_name: str):
-        if not str: raise ValueError('event_name cannot be an empty string')
+        if not event_name: raise ValueError('event_name cannot be an empty string')
         return self.events[event_name]
     
     def write_event(self, event_name: str, value: any) -> None:
         if value is None: raise ValueError('Value passed to event cannot be None')
+        if not event_name: raise ValueError('event_name cannot be an empty string')
         self.events[event_name] = value
         
