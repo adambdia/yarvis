01abc87faa03c934f76940f1006ac6fa75bd66e3
Yusuf Al-Khawaldeh <yusufkhawaldeh@gmail.com>
2025-07-08 16:45:45 -0400

    Formatted code with ruff

--- python/hand_detector.py
+++ python/hand_detector.py
@@ -6,38 +6,40 @@ from event_manager import Event_Manager
 import cv2
 import numpy as np
 
+
 class Hand_Detector:
     MP_KEY_POINTS = {
-        'THUMB_TIP': 4,
-        'THUMB_IP': 3,
-        'THUMB_MCP': 2,
-        
-        'INDEX_FINGER_TIP': 8,
-        'INDEX_FINGER_DIP': 7,
-        'INDEX_FINGER_PIP': 6,
-        'INDEX_FINGER_MCP': 5,
-        
-        'MIDDLE_FINGER_TIP': 12,
-        'MIDDLE_FINGER_DIP': 11,
-        'MIDDLE_FINGER_PIP': 10,
-        'MIDDLE_FINGER_MCP': 9,
-        
-        'RING_FINGER_TIP': 16,
-        'RING_FINGER_DIP': 15,
-        'RING_FINGER_PIP': 14,
-        'RING_FINGER_MCP': 13,
-        
-        'PINKY_TIP': 20,
-        'PINKY_DIP': 19,
-        'PINKY_PIP': 18,
-        'PINKY_MCP': 17,
-        
-        'WRIST': 0,
+        "THUMB_TIP": 4,
+        "THUMB_IP": 3,
+        "THUMB_MCP": 2,
+        "INDEX_FINGER_TIP": 8,
+        "INDEX_FINGER_DIP": 7,
+        "INDEX_FINGER_PIP": 6,
+        "INDEX_FINGER_MCP": 5,
+        "MIDDLE_FINGER_TIP": 12,
+        "MIDDLE_FINGER_DIP": 11,
+        "MIDDLE_FINGER_PIP": 10,
+        "MIDDLE_FINGER_MCP": 9,
+        "RING_FINGER_TIP": 16,
+        "RING_FINGER_DIP": 15,
+        "RING_FINGER_PIP": 14,
+        "RING_FINGER_MCP": 13,
+        "PINKY_TIP": 20,
+        "PINKY_DIP": 19,
+        "PINKY_PIP": 18,
+        "PINKY_MCP": 17,
+        "WRIST": 0,
     }
 
-    def __init__(self, event_manager: Event_Manager, detection_confidence=0.5, num_hands=1, model_path='$YARVISPATH/models/hand_landmarker.task'):
+    def __init__(
+        self,
+        event_manager: Event_Manager,
+        detection_confidence=0.5,
+        num_hands=1,
+        model_path="$YARVISPATH/models/hand_landmarker.task",
+    ):
         self.event_manager = event_manager
-        event_manager.push_event('hand_detected', False)
+        event_manager.push_event("hand_detected", False)
         self.uncalibrated_detection_result = None
         self.landmarker = None
         BaseOptions = mp.tasks.BaseOptions
@@ -45,32 +47,30 @@ class Hand_Detector:
         HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
         HandLandmarkerResult = mp.tasks.vision.HandLandmarkerResult
         VisionRunningMode = mp.tasks.vision.RunningMode
-        
+
         self._callback = partial(self._handle_result)
-        
+
         options = HandLandmarkerOptions(
-            base_options=BaseOptions(
-                model_asset_path=os.path.expandvars(model_path)
-            ),
+            base_options=BaseOptions(model_asset_path=os.path.expandvars(model_path)),
             running_mode=VisionRunningMode.LIVE_STREAM,
             result_callback=self._callback,
             min_hand_detection_confidence=detection_confidence,
-            num_hands=num_hands
+            num_hands=num_hands,
         )
         self.landmarker = HandLandmarker.create_from_options(options)
 
         self.detection_result = {}
 
-        self.calibration_matrix = False
+        self.calibration_matrix = None
         try:
-            self.calibration_matrix = np.load('calibration.npy')
-            print('calibration found')
+            self.calibration_matrix = np.load("calibration.npy")
+            print("calibration found")
         except:
-            print('no calibration found')
-  
-    
+            print("no calibration found")
 
-    def _handle_result(self, result: HandLandmarkerResult, output_image: mp.Image, time_stamp: int) -> None:
+    def _handle_result(
+        self, result: HandLandmarkerResult, output_image: mp.Image, time_stamp: int
+    ) -> None:
         """Handle the results from the hand landmarker and update event manager"""
         self.uncalibrated_detection_result = result
         self.detection_result = {}
@@ -84,26 +84,25 @@ class Hand_Detector:
                 raw_y = landmark.y * output_image.height
 
                 landmark_pos = np.array([[(raw_x, raw_y)]], dtype=np.float32)
-                landmark_pos = cv2.perspectiveTransform(landmark_pos, self.calibration_matrix)
+                landmark_pos = cv2.perspectiveTransform(
+                    landmark_pos, self.calibration_matrix
+                )
                 self.detection_result[key_point] = landmark_pos[0][0].astype(dtype=int)
 
-            
-        self.event_manager.push_event('uncalibrated_hand_result', bool(result.hand_landmarks))
-        self.event_manager.push_event('hand_result', bool(self.detection_result))
-    
+        self.event_manager.push_event(
+            "uncalibrated_hand_result", bool(result.hand_landmarks)
+        )
+        self.event_manager.push_event("hand_result", bool(self.detection_result))
 
     def detect_async(self, image: mp.Image, time_stamp: int):
         if self.landmarker:
             self.landmarker.detect_async(image, time_stamp)
-    
 
     def get_uncalibrated_result(self):
         return self.uncalibrated_detection_result
-    
+
     def get_calibrated_result(self):
         return self.detection_result
-
-    
 
     def close(self):
         if self.landmarker:
--- python/kinect_manager.py
+++ python/kinect_manager.py
@@ -1,11 +1,11 @@
 import cv2
 import numpy as np
-from kinect_bridge import KinectBridge 
+from kinect_bridge import KinectBridge
 import asyncio
 import queue
 
+
 class Kinect:
-    
     def __init__(self, event_manager) -> None:
         try:
             print("Initializing Kinect...")
@@ -17,28 +17,24 @@ class Kinect:
         self.event_manager = event_manager
         self.depth_frame = None
         self.ir_frame = None
-        
-
 
     def update_frames(self) -> None:
-                try:
-                    frames = self.kinect.get_frames()
-                    # bgr_frame = frames['bgr']
-                    depth_frame = frames['depth']
-                    ir_frame = frames['ir']              
-                    ir_frame = ir_frame / 256.0
-                    ir_frame = ir_frame.astype(np.uint8)
-                    ir_frame = cv2.cvtColor(ir_frame, cv2.COLOR_GRAY2BGR)
-                    ir_frame = cv2.flip(ir_frame, 0)
-                    self.depth_frame = depth_frame
-                    self.ir_frame = ir_frame
-                except RuntimeError as e:
-                    print(e)
-    
+        try:
+            frames = self.kinect.get_frames()
+            # bgr_frame = frames['bgr']
+            depth_frame = frames["depth"]
+            ir_frame = frames["ir"]
+            ir_frame = ir_frame / 256.0
+            ir_frame = ir_frame.astype(np.uint8)
+            ir_frame = cv2.cvtColor(ir_frame, cv2.COLOR_GRAY2BGR)
+            ir_frame = cv2.flip(ir_frame, 0)
+            self.depth_frame = depth_frame
+            self.ir_frame = ir_frame
+        except RuntimeError as e:
+            print(e)
+
     def get_ir_frame(self):
         return self.ir_frame
 
     def get_depth_frame(self):
         return self.depth_frame
-
-    
